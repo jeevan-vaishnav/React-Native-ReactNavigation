e4bf7de8fdf395ca3fe5ea0e39d70e2c1a3fca59
Jeevan0109 <jeevanvaishnavv@gmail.com>
2021-11-16 18:56:32 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,30 @@
+import "react-native-gesture-handler";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./Screens/Home";
+import ContactScreen from "./Screens/Contact";
+import AboutScreen from "./Screens/Contact";
+
+export default function App() {
+  const Stack = createStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
+        <Stack.Screen name="Contact" component={ContactScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
